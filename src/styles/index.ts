4cf1032227d054ce07b8243e67277aabe73c8230
Andrew Gerez <andrewgerez18@gmail.com>
2023-08-13 18:03:10 -0300

    Feature: 5 | global styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -12,7 +12,15 @@ export const {
 } = createStitches({
   theme: {
     colors: {
-      dracoshop: '#8257e6',
+      white: '#FFF',
+
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#c4c4cc',
+      gray100: '#e1e1e6',
+
+      red500: '#600b04',
+      red300: '#900603',
     }
   }
 })
