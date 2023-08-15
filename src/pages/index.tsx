582af58ba10ff31de7bb5b25d28a00dc6f1e6ba8
Andrew Gerez <andrewgerez18@gmail.com>
2023-08-14 21:31:51 -0300

    Feature: 8 | use keen slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,21 @@ import shirt1 from '../assets/shirts/1.svg'
 import shirt2 from '../assets/shirts/2.svg'
 import shirt3 from '../assets/shirts/3.svg'
 import shirt4 from '../assets/shirts/4.svg'
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
 
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={shirt1} width={520} height={480} alt="" />
 
         <footer>
@@ -17,8 +26,24 @@ export default function Home() {
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={shirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt4} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta X</strong>
