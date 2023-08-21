b21b2ea817b056c0b30bdbc0528a68075a91fd91
Andrew Gerez <andrewgerez18@gmail.com>
2023-08-20 22:12:47 -0300

    Feature: 23 | ssr redirect in sucess page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -14,12 +14,12 @@ interface ISuccess {
 }
 
 export default function Success({ customerName, product }: ISuccess) {
-  return(
+  return (
     <SuccessContainer>
       <h1>Compra efetuada!</h1>
 
       <ImageContainer>
-        <Image 
+        <Image
           src={product.imageUrl}
           width={120}
           height={110}
@@ -39,6 +39,15 @@ export default function Success({ customerName, product }: ISuccess) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
+
   const sessionId = String(query.session_id);
 
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
@@ -47,7 +56,7 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
 
   const customerName = session.customer_details.name;
   const product = session.line_items.data[0].price.product as Stripe.Product;
-  
+
   return {
     props: {
       customerName,
