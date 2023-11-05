6128507f3becde93962f14ee2c97941363c02f99
Andrew Gerez <andrewgerez18@gmail.com>
2023-11-04 23:04:53 -0300

    feat: store cta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { globalStyles } from "../styles/global"
 import logoImage from '../assets/logo.png'
 import Image from "next/image"
 import { Container, Header } from "../styles/pages/app"
+import { Bag } from "phosphor-react";
 
 globalStyles()
 
@@ -16,6 +17,9 @@ export default function App({ Component, pageProps }: AppProps) {
           src={logoImage.src} 
           alt="" 
         />
+        <button>
+          <Bag size={24} />
+        </button>
       </Header>
 
       <Component {...pageProps} />
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -8,8 +8,33 @@ export const Container = styled('div', {
 })
 
 export const Header = styled('header', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+
   padding: '3rem 0',
   width: '100%',
   maxWidth: 1180,
   margin: '0 auto',
+
+  button: {
+    all: 'unset',
+    padding: '0.75rem',
+    borderRadius: '0.375rem',
+    backgroundColor: '$gray800',
+    lineHeight: 0,
+
+    svg: {
+      color: '$gray500',
+      transition: '0.3s all',
+    },
+
+    '&:hover': {
+      cursor: 'pointer',
+
+      svg: {
+        color: '$purple300',
+      }
+    }
+  },
 })
